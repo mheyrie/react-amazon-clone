5be5b16e1c90baf4c66d3392a097858950ed3cb4
Mheyrie <omota4fidelity@gmail.com>
2024-06-24 23:56:29 +0100

    Using context Api to add item to basket

--- src/component/Checkout.js
+++ src/component/Checkout.js
@@ -3,8 +3,10 @@ import '../styles/Checkout.css'
 import ad from '../image/ad.webp'
 import CheckoutProduct from "./CheckoutProduct"
 import Subtotal from "./Subtotal"
+import {useStateValue} from '../StateProvider/StateProvider';
 
 function Checkout() {
+  const [{basket}] = useStateValue();
   return (
     <div className="checkout">
         <div className="checkout_left">
@@ -12,8 +14,15 @@ function Checkout() {
             <h2 className="checkout_title">
                 Your Shopping Basket
             </h2>
-            <CheckoutProduct/>
-            <CheckoutProduct/>
+            {basket.map(item=> (
+              <CheckoutProduct
+              id={item.id}
+              title={item.title}
+              image={item.image}
+              price={item.price}
+              rating={item.rating}
+              />
+            ))}
         </div>
         <div className="checkout_right">
             <Subtotal/>
