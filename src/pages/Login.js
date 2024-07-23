3a9110a27a36940111f28a300796eeffe57d75c9
Mheyrie <omota4fidelity@gmail.com>
2024-07-23 02:26:12 +0100

    Css file for login

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,5 +1,7 @@
 import React, { useState } from 'react'
-import { Link } from "react-router-dom"
+import { Link, useHistory } from "react-router-dom"
+import StorefrontIcon from "@mui/icons-material/Storefront";
+import '../styles/Login.css'
 
 function Login() {
     const [email, setEmail] = useState('')
@@ -14,7 +16,22 @@ function Login() {
         </div>
         </Link>
         <div className="login_container">
-            
+            <h1>sign-in</h1>
+            <form action="">
+                <h5>Email</h5>
+                <input type="text" value={email} onChange={e => setEmail(e.target.value)} />
+
+                <h5>Password</h5>
+                <input type="password" value={password} onChange={e=>setPassword(e.target.value)}/>
+
+                <button className="login_signin_button" type="submit">Sign In</button>
+            </form>
+
+            <p>
+                Lorem ipsum, dolor sit amet consectetur adipisicing elit. Molestias laboriosam reprehenderit beatae vitae quas pariatur recusandae voluptates fugit itaque autem, odio voluptatibus iure in quibusdam asperiores repellat tempore necessitatibus aliquid?
+            </p>
+
+            <button className="login_reg_button">Create you eSHOP</button>
         </div>
     </div>
   )
