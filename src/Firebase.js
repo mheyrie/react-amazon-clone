7311c1b3d6ff16adf26981e27aea0898f5b93b29
Mheyrie <omota4fidelity@gmail.com>
2024-07-23 03:05:15 +0100

    Setting up firebase for login

---
+++ src/Firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyAdylucj2_vFgTOjr4vEGntKU8w9Q_-O_0",
+    authDomain: "clone-eshop.firebaseapp.com",
+    projectId: "clone-eshop",
+    storageBucket: "clone-eshop.appspot.com",
+    messagingSenderId: "125712700017",
+    appId: "1:125712700017:web:23c8660ea528bb8e6b81d0",
+    measurementId: "G-X8JQEP8B1Z"
+  };
